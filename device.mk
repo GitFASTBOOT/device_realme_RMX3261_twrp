f4cffcca4161f2efa8abc2a4a5fb329772c9d25f
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-06 00:09:08 -0500

    RMX3261: Define VNDK

--- device.mk
+++ device.mk
@@ -15,6 +15,12 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
