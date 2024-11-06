11395021be45a30f150c6bea920c1a99bea53939
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-06 06:11:50 +0200

    RMX3261: Initial TWRP tree

---
+++ BoardConfig.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/realme/RMX3261
+
+
+# Platform
+TARGET_BOARD_PLATFORM := ums512
+
+
