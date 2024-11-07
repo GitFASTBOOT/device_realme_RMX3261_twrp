bd33111a6be0095ef136e097f4dc2c99286fe562
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-06 19:38:28 -0500

    RMX3261: Correct version

--- device.mk
+++ device.mk
@@ -32,11 +32,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 
-# Boot control HAL
+# Boot Control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-service
 
 # bootctrl HAL    
 PRODUCT_PACKAGES += \
